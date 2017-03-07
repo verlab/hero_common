29db21c9d19bcfd266729d4fe5e9b48e3b6aab92
Rezeck <rezeck@dcc.ufmg.br>
2017-03-07 17:17:32 -0300

    Working on reader/write mode with interrupt signal

--- firmware/swarm/robot/robot.ino
+++ firmware/swarm/robot/robot.ino
@@ -1,7 +1,7 @@
 /*
- ********************************   
+ ************************************************************************
  *  R O B O T  F I R M W A R E
- ********************************
+ ************************************************************************
  *
  * Paulo Rezeck <rezeck@dcc.ufmg.br>
  * Hector Azpurua <hectorxxx@gmail.com>
@@ -9,112 +9,150 @@
  * Project Swarm
  * Computer Vision and Robotics Lab
  * Federal University of Minas Gerais - Brazil
- */
-
+ ************************************************************************/
+ 
+/************************************************************************/
 #include <SPI.h>
 #include "nRF24L01.h"
-#include "RF24.h"
+#include "RF24.h" // 
 #include "printf.h"
-
 #include <Servo.h>
-/*
- * Robot ID
- */
-#define ID 007
+/************************************************************************/
 
-/*
+/************************************************************************
+ * Robot ID
+ ************************************************************************/
+uint8_t id = 11;
+/************************************************************************/
+
+/************************************************************************
  * Protocol
- */
+ ************************************************************************/
  struct Protocol{
     uint8_t robotId;
     uint8_t data1;
     uint8_t data2;
 };
+/************************************************************************/
 
-/*
+/************************************************************************
  * Hardware Configuration
  * Set up nRF24L01 radio on SPI bus plus pins 9 & 10
- */
+ * <http://maniacbug.github.io/RF24/classRF24.html>
+ ************************************************************************/
 RF24 radio(9,10);
+/************************************************************************/
 
-/* 
+/************************************************************************
  * Topology 
  * Radio pipe addresses for the 2 nodes to communicate.
- */
+ ************************************************************************/
 const uint64_t address[] = {0xF0F0F0F0D2LL, 0xF0F0F0F0E1LL};
+/************************************************************************/
 
-/*
- * Setup time to write data to the pipe
- */
-unsigned long writeTime, timeout = 200;
-Servo wheelR;
+/************************************************************************
+ * Interrupt handler, check the radio because we got an IRQ
+ ************************************************************************/
+void check_radio(void);
+/************************************************************************/
 
+/************************************************************************
+ * Servo motor driver
+ ************************************************************************/
+Servo wheel_right, wheel_left;
+/************************************************************************/
+
+/************************************************************************
+ * Arduino Setup
+ ************************************************************************/
 void setup() {
+  /* Serial initialization */
   Serial.begin(57600);
   printf_begin();
   printf("[Status] Starting Robot Node\n");
-  wheelR.attach(6);
 
-  // Setup and configure rf radio
+  /* Setup servo motor */
+  wheel_right.attach(6);
+  wheel_right.write(98);
+
+  /* Setup and configure rf radio */
   radio.begin();
 
-  // Set the PA Level low to prevent power supply related issues since this is a
-  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  /* Set the PA Level low to prevent power supply related issues since this is a
+   * getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+   */
   radio.setPALevel(RF24_PA_LOW);
   
-  // optionally, increase the delay between retries & # of retries
+  /* optionally, increase the delay between retries & # of retries */
   //radio.setRetries(15,15);
 
-  // optionally, reduce the payload size.  seems to improve reliability
-  radio.setPayloadSize(sizeof(Protocol));
+  /* optionally, reduce the payload size.  seems to improve reliability */
+  //radio.setPayloadSize(sizeof(Protocol));
 
-  // Open pipes to other nodes for communication
-  delay(150);
-  //radio.openWritingPipe(write_pipe);
-  //delay(15);
-  
-  radio.openReadingPipe(1, address[1]);
+  /* Open pipes to other nodes for communication */
+  radio.openWritingPipe(address[1]);
+  delay(15);
+  radio.openReadingPipe(1, address[0]);
   delay(15);
 
-  // Start listening
+  /* Start listening */
   radio.startListening();
 
-  // Dump the configuration of the rf unit for debugging
+  /* Dump the configuration of the rf unit for debugging */
   radio.printDetails();
-  //delay(5000);
 
-  // Get current time configuration to write
-  writeTime = millis();
+  /* Attach interrupt handler to interrupt #0 (using pin 2) on BOTH the sender and receiver */
+  attachInterrupt(0, check_radio, FALLING);
 }
+/************************************************************************/
 
+/************************************************************************
+ * M A I N  L O 0 P
+ ************************************************************************/
 void loop() {
-  // if there is data ready
-  while ( radio.available()){ //&& (millis() - writeTime) <= timeout){
+  /* First, stop listening so we can talk. */
+  //radio.stopListening();
+  
+  /* Non-blocking write to the open writing pipe. */
+  //radio.startWrite(&id, sizeof(uint8_t));
+  
+  /* Call this when you get an interrupt to find out why. 
+   * Tells you what caused the interrupt, and clears the state of interrupts. 
+   */
+  //bool tx_ok, tx_fail, rx_ready;
+  //radio.whatHappened  ( tx_ok, tx_fail, rx_ready );
+  //printf("[Status] %d %d %d\n", tx_ok, tx_fail, rx_ready);
+  
+  /* Now, resume listening so we catch the next packets. */
+  //radio.startListening();
+}
+/************************************************************************/
+
+/************************************************************************
+ * C A L L B A C K S
+ ************************************************************************/
+void check_radio(void){
+  /* Check the read pipe for message */
+  if ( radio.available()){
+    /* Read the message */
     Protocol msg;
     radio.read( &msg, sizeof(Protocol));
-    if (msg.robotId != ID){ // msg isn't to me
-      break;
+
+    /* Check if it is to this robot */
+    if (msg.robotId != id){ // message isn't to me
+      printf("[Status] Msg isn't to me!\n");
+      return;
     }
+    
+    /* Control the wheels */
+    wheel_right.write((int)msg.data1);
+    
     printf("----------------------\n");
     printf("[Status] Become data:\n");  
     printf("[Status] Data: {%d, %d, %d}\n", msg.robotId, msg.data1, msg.data2);
-    wheelR.write((int)msg.data1);
     printf("----------------------\n");
   }
-  //else{
-    // First, stop listening so we can talk.
-//    radio.stopListening();
-//
-//     // Send the control data.
-//     printf("Sending control datas...\n");
-//     Protocol msg = {0, 0, 90, 90};
-//     radio.write( &msg, sizeof(Protocol) );
-//
-//    // Now, resume listening so we catch the next packets.
-//    radio.startListening();
-
-    //writeTime = millis();
-  //}
-  // Sync time
-  delay(2);
 }
+/************************************************************************/
+/* T H E  E N D */
+/************************************************************************/
