56af555ce069d232c52cd6d2c6ba734aab27828b
h3ct0r <h3ct0r.ml@gmail.com>
2017-05-30 13:07:15 -0300

    reverted old control algo

--- firmware/swarm/robotWiFi/robotWiFi.ino
+++ firmware/swarm/robotWiFi/robotWiFi.ino
@@ -33,8 +33,9 @@ char* WIFI_PASSWD = "hero_network";
 #define WHEEL_DIAMETER 4.9
 #define WHEEL_SEPARATION 8
 float WHEEL_CIRCUNFERENCE = 3.14 * WHEEL_DIAMETER;
-#define WHEEL_R_MID 1575  // Neutral position of right motor
-#define WHEEL_L_MID 1570  // Neutral position of left motor
+#define WHEEL_R_MID 1595  // Neutral position of right motor
+#define WHEEL_L_MID 1585  // Neutral position of left motor
+#define GAMMA_GAIN_MOTORS 15
 
 /* Robot Identification Setup */
 #define ROBOT_NAME  "/hero_" ROBOT_ID
@@ -48,8 +49,8 @@ float WHEEL_CIRCUNFERENCE = 3.14 * WHEEL_DIAMETER;
 
 /* LED Setup */
 #define IR1_PIN 4//0 /* Proximity sensor 1 Pin D3 */
-#define IR2_PIN 0//2 /* Proximity sensor 1 Pin D4 */
-#define IR3_PIN 2//14 /* Proximity sensor 1 Pin D5 */
+#define IR2_PIN 13//2 /* Proximity sensor 1 Pin D4 */
+#define IR3_PIN 12//14 /* Proximity sensor 1 Pin D5 */
 /************************************************************************/
 
 /************************************************************************
@@ -105,19 +106,6 @@ void setup() {
   Serial.println("\t-Hector Azpurua hector.azpurua@dcc.ufmg.br");
   Serial.println("");
 
-  if(DEBUG) Serial.println("Init networking ...");
-  nh.getHardware()->setROSConnection(ROS_MASTER_ADDRESS, ROS_MASTER_PORT);
-  nh.getHardware()->connectWifi(WIFI_SSID, WIFI_PASSWD);
-  
-  /* Starting ros node */
-  if(DEBUG) Serial.println("Init ros node ...");
-  nh.initNode();
-  nh.advertise(proximity_pub);
-  nh.subscribe(velocity_sub);
-  nh.subscribe(led_sub);
-
-  proximity_msg.header.frame_id = ROBOT_NAME;
-
   /* Setup servo motor */
   if(DEBUG) Serial.println("Setup servos ...");
   wheel_right.attach(WHEEL_R_PIN);
@@ -131,6 +119,19 @@ void setup() {
   pinMode(IR1_PIN, INPUT); 
   pinMode(IR2_PIN, INPUT);
   pinMode(IR3_PIN, INPUT);
+
+  if(DEBUG) Serial.println("Init networking ...");
+  nh.getHardware()->setROSConnection(ROS_MASTER_ADDRESS, ROS_MASTER_PORT);
+  nh.getHardware()->connectWifi(WIFI_SSID, WIFI_PASSWD);
+  
+  /* Starting ros node */
+  if(DEBUG) Serial.println("Init ros node ...");
+  nh.initNode();
+  nh.advertise(proximity_pub);
+  nh.subscribe(velocity_sub);
+  nh.subscribe(led_sub);
+
+  proximity_msg.header.frame_id = ROBOT_NAME;
 
   /* Setup complete LED message */
   pinMode(LED_BUILTIN, OUTPUT);
@@ -166,55 +167,26 @@ void loop(){
 void velocity_cb(const geometry_msgs::Twist& msg){
   /* set motor velocities */
 
-//  float linear = _max(_min(msg.linear.x, WHEEL_DIAMETER), -WHEEL_DIAMETER);
-//  float angular = _max(_min(msg.angular.z, 2*WHEEL_DIAMETER/WHEEL_SEPARATION), -2*WHEEL_DIAMETER/WHEEL_SEPARATION);
-//
-//  float wl0 = (linear - ((float) WHEEL_SEPARATION/2.0) * angular) / (float) WHEEL_DIAMETER;
-//  float wr0 = (linear + ((float) WHEEL_SEPARATION/2.0) * angular) / (float) WHEEL_DIAMETER;
-//
-//  float wr = _map2(wr0, 0, 0.5, -500, 500);
-//  float wl = _map2(wl0, -2.0, 2.0, -500, 500);
-//  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));   // Turn the LED on (Note that LOW is the voltage level
+  float linear = _max(_min(msg.linear.x, WHEEL_DIAMETER), -WHEEL_DIAMETER);
+  float angular = _max(_min(msg.angular.z, 2*WHEEL_DIAMETER/WHEEL_SEPARATION), -2*WHEEL_DIAMETER/WHEEL_SEPARATION);
 
-  // m/s to m/min
-  int cV = 1000; // multiplier of the velocities
-  float linear_vel_x_mins_ = msg.linear.x * cV;
-  float linear_vel_y_mins_ = msg.linear.y * cV;
+  float wl0 = (linear - ((float) WHEEL_SEPARATION/2.0) * angular) / (float) WHEEL_DIAMETER;
+  float wr0 = (linear + ((float) WHEEL_SEPARATION/2.0) * angular) / (float) WHEEL_DIAMETER;
 
-  // convert rad/s to rad/min
-  float angular_vel_z_mins_ = msg.angular.z * cV;
+  float wr = _map2(wr0, -2.0, 2.0, -500, 500);
+  float wl = _map2(wl0, -2.0, 2.0, -500, 500);
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));   // Turn the LED on (Note that LOW is the voltage level
 
-  // Vt = w * radius
-  float tangential_vel_ = angular_vel_z_mins_ * WHEEL_SEPARATION;
-
-  float x_rpm_ = linear_vel_x_mins_ / WHEEL_CIRCUNFERENCE;
-  float y_rpm_ = linear_vel_y_mins_ / WHEEL_CIRCUNFERENCE;
-  float tan_rpm_ = tangential_vel_ / WHEEL_CIRCUNFERENCE;
-
-  // calculate for the target motor RPM and direction
-  // front-left motor
-  float m1 = x_rpm_ - y_rpm_ - tan_rpm_;
-
-  // front-right motor
-  float m2 = x_rpm_ + y_rpm_ + tan_rpm_;
-
-  if(DEBUG){
-    Serial.print("linear_vel_x_mins_: ");
-    Serial.println(linear_vel_x_mins_);
-
-    Serial.print("angular_vel_z_mins_: ");
-    Serial.println(angular_vel_z_mins_);
-
-    Serial.print("m1: ");
-    Serial.println(m1);
-    Serial.print("m2: ");
-    Serial.println(m2);
-    Serial.println("");
-  }
-  
   /* Control the wheels */
-  wheel_right.writeMicroseconds(int(WHEEL_R_MID) + (int)m1);
-  wheel_left.writeMicroseconds(int(WHEEL_L_MID) - (int)m2);
+  wheel_right.writeMicroseconds(int(WHEEL_R_MID) - (int) wr);
+  wheel_left.writeMicroseconds(int(WHEEL_L_MID) + (int) wl);
+}
+
+float _map2(float x, int xmin, int xmax, int ymin, int ymax){
+  float v = (((x -xmin) / float (xmax-xmin)) * (abs(ymax) + abs(ymin))) - abs(ymin);
+  if(v < ymin) v = ymin;
+  if(v > ymax) v = ymax;
+  return v;
 }
 
 void led_cb(const swarm_driver::Led& msg){
