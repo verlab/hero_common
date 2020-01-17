62b529d63cef11a2f0cdcca953f4a260b9bd10d7
Paulo Rezeck <rezeck@dcc.ufmg.br>
2020-01-17 12:41:26 -0300

    Added ir and neopixel test; enable rosserial server to fork.

--- hero_firmware/tests/ir_test/ir_test.ino
+++ hero_firmware/tests/ir_test/ir_test.ino
@@ -18,23 +18,13 @@
 
 #include <ESP8266WiFi.h>
 
-#include <NeoPixelBus.h>
-#include <NeoPixelBrightnessBus.h>
-#define colorSaturation 128
-
-const uint16_t PixelCount = 7; // this example assumes 4 pixels, making it smaller will cause a failure
-NeoPixelBrightnessBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> strip(PixelCount);
-RgbColor red(colorSaturation, 0, 0);
-RgbColor green(0, colorSaturation, 0);
-RgbColor black(0);
-
 /* Timer */
 double timer, log_timer, rate = 50;
 
 /* Wifi setup */
-IPAddress ROS_MASTER_ADDRESS(10, 42, 0, 1); // ros master ip
-char* WIFI_SSID = "epuck_net"; // network name
-char* WIFI_PASSWD = "epuck_9895"; // network password
+IPAddress ROS_MASTER_ADDRESS(150, 164, 212, 125); // ros master ip
+char* WIFI_SSID = "Verlab"; // network name
+char* WIFI_PASSWD = "Verlab$Router"; // network password
 
 /* ROS Setup */
 String hero_name;
@@ -49,11 +39,6 @@ String laser_frame;
 void update_laser(void);
 String laser_topic;
 ros::Publisher *laser_pub;
-
-/* LED callback */
-void led_callback(const std_msgs::ColorRGBA& msg);
-String led_topic;
-ros::Subscriber<std_msgs::ColorRGBA> *led_sub;
 
 char buf [100];
 
@@ -80,9 +65,6 @@ void setup() {
   laser_topic = hero_name + String("/laser");
   laser_pub = new ros::Publisher(laser_topic.c_str(), &laser_msg);
 
-  led_topic = hero_name + String("/led");
-  led_sub = new ros::Subscriber<std_msgs::ColorRGBA>(led_topic.c_str(), led_callback);
-
   /* Starting ros node */
   nh.initNode();
   
@@ -101,44 +83,18 @@ void setup() {
   laser_msg.intensities_length = 8;
   laser_msg.intensities = (float *)malloc(8 * sizeof(float));
 
-  /* Address Subscribers */
-  nh.subscribe(*led_sub);
-
   /* Setup Analog Multiplexer Pins */
   pinMode(s0, OUTPUT); 
   pinMode(s1, OUTPUT);
   pinMode(s2, OUTPUT);
   pinMode(enable, OUTPUT);
   readIR();
-
-  /* Setup complete esp LED message */
-  // this resets all the neopixels to an off state
-  strip.Begin();
-  strip.Show();
-  for(int i = 0; i < PixelCount; i++){
-    strip.SetPixelColor(i, black);
-  }
-  strip.SetBrightness(64);
-  strip.Show();
   
   pinMode(LED_BUILTIN, OUTPUT);
-  for(int i = 0; i <= 7 * 4; i++){
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    if (i % 8 == 0) continue;
-    if (laser_msg.ranges[0] > 900)
-      strip.SetPixelColor(i % 8, green);
-    else
-      strip.SetPixelColor(i % 8, red);
-    strip.SetBrightness(16);
-    strip.Show();
-    delay(100);                   
-    strip.SetPixelColor(i % 8, black);
-  }
-  strip.Show();
   
   /* ROS LOG */
   //Serial.println("\n\n Welcome to hero odometry test! ");
-  sprintf(buf,"\33[96m Welcome to Hero #1 - IR Test! \33[0m");
+  sprintf(buf,"\33[96m Welcome to Hero test mode - IR Test! \33[0m");
   nh.loginfo(buf);
 }
 
@@ -151,14 +107,13 @@ void loop() {
     /* ROS INFOS */
     if ((millis() - log_timer) > 3000){
       log_timer = millis();
-      sprintf(buf,"\33[96m[HERO-1] Conected at time %d\33[0m", millis());
+      sprintf(buf,"\33[96m[HERO-Test] Conected at time %d\33[0m", millis());
       nh.loginfo(buf);
     }
 
     /* ROS Loop */
     if (millis() - timer > rate){ 
       timer = millis();
-      //update_odom();
       readIR();
       nh.spinOnce();
     }
@@ -196,17 +151,5 @@ void readIR(){
   laser_msg.header.stamp = nh.now();
   laser_pub->publish( &laser_msg ); 
 
-}
-
-
-void led_callback(const std_msgs::ColorRGBA& msg){
-  strip.Begin();
-  RgbColor color(msg.r, msg.g, msg.b);
-  for(int i = 0; i <= 7; i++){
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    strip.SetPixelColor(i, color);
-  }
-  strip.SetBrightness(msg.a);
-  strip.Show();
 }
 
--- hero_firmware/tests/neopixel_test/neopixel_test.ino
+++ hero_firmware/tests/neopixel_test/neopixel_test.ino
@@ -1,164 +1,135 @@
+ /************************************************************************
+ *  H E R O   N E O   P I X E L   T E S T
+ ************************************************************************
+ *
+ * Paulo Rezeck <rezeck@dcc.ufmg.br>
+ * Mauricio Ferrari <mauferrari@dcc.ufmg.br>
+ * Hector Azpurua <hectorxxx@gmail.com>
+ * 
+ * HeRo Project
+ * Computer Vision and Robotics Lab
+ * Federal University of Minas Gerais - Brazil
+ ************************************************************************/
+#include <ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <std_msgs/ColorRGBA.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+
+#include <ESP8266WiFi.h>
+
 #include <NeoPixelBus.h>
 #include <NeoPixelBrightnessBus.h>
+#define colorSaturation 255
+
 const uint16_t PixelCount = 7; // this example assumes 4 pixels, making it smaller will cause a failure
-const uint8_t PixelPin = 16;  // make sure to set this to the correct pin, ignored for Esp8266
-
-#define colorSaturation 128
-
-//NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> strip(PixelCount, PixelPin);
 NeoPixelBrightnessBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> strip(PixelCount);
 RgbColor red(colorSaturation, 0, 0);
 RgbColor green(0, colorSaturation, 0);
-RgbColor blue(0, 0, colorSaturation);
-RgbColor white(colorSaturation);
-RgbColor yellow(255, 255, 50);
-RgbColor green2(73, 153, 0);
-RgbColor pink(255, 0, 127);
 RgbColor black(0);
-RgbColor one(0, 0, 1);
 
+/* Timer */
+double timer, log_timer, rate = 50;
+
+/* Wifi setup */
+IPAddress ROS_MASTER_ADDRESS(150, 164, 212, 125); // ros master ip
+char* WIFI_SSID = "Verlab"; // network name
+char* WIFI_PASSWD = "Verlab$Router"; // network password
+
+/* ROS Setup */
+String hero_name;
+/* ROS Node Instaciatation */
+ros::NodeHandle nh;
+
+/* LED callback */
+void led_callback(const std_msgs::ColorRGBA& msg);
+String led_topic;
+ros::Subscriber<std_msgs::ColorRGBA> *led_sub;
+
+char buf [100];
+
+/* Setup */
 void setup() {
-  // put your setup code here, to run once:
-    Serial.begin(115200);
-    while (!Serial); // wait for serial attach
-    Serial.println();
-    Serial.println("Initializing...");
-    Serial.flush();
-
-    // this resets all the neopixels to an off state
-    strip.Begin();
-    strip.Show();
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, black);
-      }
-      strip.SetBrightness(64);
-      strip.Show();
-    
-      
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  strip.Begin();
-  Serial.println("Setting blue");
-  for(int i = 0; i < PixelCount; i++){
-    strip.SetPixelColor(i, blue);
-  }
-  strip.SetBrightness(255);
-  strip.Show();
-  delayMicroseconds(1000);
+  /* Connect the ESP8266 the the wifi AP */
+  WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
   
-  Serial.println("Setting red");
-  for(int i = 0; i < PixelCount; i++){
-    strip.SetPixelColor(i, red);
-  }
-  strip.SetBrightness(255);
+  Serial.begin(115200);
+
+  /* Start ROS communication module */
+  uint16_t ROS_MASTER_PORT = 11411;
+  nh.getHardware()->setConnection(ROS_MASTER_ADDRESS, ROS_MASTER_PORT);
+
+  /* Setup laser publisher */
+  hero_name = String("/hero_1");
+  
+  led_topic = hero_name + String("/led");
+  led_sub = new ros::Subscriber<std_msgs::ColorRGBA>(led_topic.c_str(), led_callback);
+
+  /* Starting ros node */
+  nh.initNode();
+  
+  /* Address Subscribers */
+  nh.subscribe(*led_sub);
+
+  /* Setup complete esp LED message */
+  // this resets all the neopixels to an off state
+  strip.Begin();
   strip.Show();
-  delayMicroseconds(1000);
-
-  for(int i = 0; i < PixelCount; i++){
-    Serial.println("Setting green");
-    strip.SetPixelColor(i, green);
+  for(int i = 0; i <= PixelCount; i++){
+    strip.SetPixelColor(i, black);
   }
-  strip.SetBrightness(255);
+  strip.SetBrightness(64);
   strip.Show();
-  delayMicroseconds(1000);
-
-  Serial.println("Setting output");
-  for (int i = 0; i < 10; i++){
-      pinMode(2, OUTPUT);
-      digitalWrite(2, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(2, LOW);
-      delayMicroseconds(10);
+  
+  //pinMode(LED_BUILTIN, OUTPUT);
+  for(int i = 0; i <= PixelCount * 4; i++){
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    if (i % 8 == 0) continue;
+      strip.SetPixelColor(i % 8, green);
+    strip.SetBrightness(16);
+    strip.Show();
+    delay(100);                   
+    strip.SetPixelColor(i % 8, black);
   }
-    /*for(int i = 0; i < PixelCount; i++){
-       strip.SetPixelColor(i, white);
-      }
-      strip.SetBrightness(255);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, red);
-      }
-      strip.SetBrightness(255);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, green);
-      }
-      strip.SetBrightness(255);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, blue);
-      }
-      strip.SetBrightness(255);
-      strip.Show();
-      delay(5000);
-
-
-    for(int i = 0; i < PixelCount; i++){
-       strip.SetPixelColor(i, white);
-      }
-      strip.SetBrightness(128);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, red);
-      }
-      strip.SetBrightness(128);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, green);
-      }
-      strip.SetBrightness(128);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, blue);
-      }
-      strip.SetBrightness(128);
-      strip.Show();
-      delay(5000);
-
-
-     for(int i = 0; i < PixelCount; i++){
-       strip.SetPixelColor(i, white);
-      }
-      strip.SetBrightness(64);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, red);
-      }
-      strip.SetBrightness(64);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, green);
-      }
-      strip.SetBrightness(64);
-      strip.Show();
-      delay(5000);
-
-    for(int i = 0; i < PixelCount; i++){
-      strip.SetPixelColor(i, blue);
-      }
-      strip.SetBrightness(64);
-      strip.Show();
-      delay(5000);
-
-*/
-
-   
+  strip.Show();
+  
+  /* ROS LOG */
+  //Serial.println("\n\n Welcome to hero odometry test! ");
+  sprintf(buf,"\33[96m Welcome to Hero test mode - NeoPixel Test! \33[0m");
+  nh.loginfo(buf);
 }
+
+
+
+/************************************************************************
+ * M A I N  L O 0 P
+ ************************************************************************/
+void loop() { 
+    /* ROS INFOS */
+    if ((millis() - log_timer) > 3000){
+      log_timer = millis();
+      sprintf(buf,"\33[96m[HERO-Test] Conected at time %d\33[0m", millis());
+      nh.loginfo(buf);
+    }
+
+    /* ROS Loop */
+    if (millis() - timer > rate){ 
+      timer = millis();
+      nh.spinOnce();
+      
+    }
+}
+
+void led_callback(const std_msgs::ColorRGBA& msg){
+  //strip.Begin();
+  //delay(50);
+  RgbColor color(msg.r, msg.g, msg.b);
+  for(int i = 0; i <= PixelCount; i++){
+    //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    strip.SetPixelColor(i, color);
+  }
+  strip.SetBrightness(msg.a);
+  strip.Show();
+}
+
