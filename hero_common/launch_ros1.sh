3f394c8db07f69e1fbee0634368e58c16a4b10b4
Paulo Rezeck <rezeck@dcc.ufmg.br>
2024-02-24 18:28:48 -0300

    Update ros1 docker launch script

--- hero_common/launch_ros1.sh
+++ hero_common/launch_ros1.sh
@@ -4,11 +4,11 @@ xhost +
 
 docker run --rm -it --privileged --net=host \
     --gpus all \
-    -e NVIDIA_VISIBLE_DEVICES=0 \
-    --env=DISPLAY \
+    --env=NVIDIA_VISIBLE_DEVICES=all \
     --env=NVIDIA_DRIVER_CAPABILITIES=all \
+    --env=DISPLAY \
     --env=QT_X11_NO_MITSHM=1 \
-    --volume=$HOME/.Xauthority:/root/.Xauthority:rw \
     --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --volume=$HOME/.Xauthority:/root/.Xauthority:rw \
     --volume=$PWD/../../hero_common:/catkin_ws/src/hero_common:rw \
     --runtime=nvidia hero_common:ros1-noetic
